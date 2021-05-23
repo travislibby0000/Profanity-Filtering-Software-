ed4ddc24fb3f4604513c96a1c126a40fa600e41e
travislibby0000 <82604516+travislibby0000@users.noreply.github.com>
2021-05-22 17:49:22 -0700

    Create censorCharacter.js

---
+++ censorCharacter.js
@@ -0,0 +1,20 @@
+
+export default class Config {
+  censorCharacter: string;
+  censorFixedLength: number;
+  defaultSubstitution: string;
+  defaultWordMatchMethod: number;
+  defaultWordRepeat: boolean;
+  defaultWordSeparators: boolean;
+  filterMethod: number;
+  filterWordList: boolean;
+  iWordWhitelist: string[];
+  preserveCase: boolean;
+  preserveFirst: boolean;
+  preserveLast: boolean;
+  showCounter: boolean;
+  showSummary: boolean;
+  substitutionMark: boolean;
+  wordlistId: number;
+  wordlists: string[];
+  wordlistsEnabled
