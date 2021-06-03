ece5ea44f9940a79606a1b6fbd6d7b524207c555
travislibby0000 <82604516+travislibby0000@users.noreply.github.com>
2021-06-03 13:16:54 -0700

    Create wordlist.spec.js

---
+++ Test/Lib/wordlist.spec.js
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+import Constants from '../built/lib/constants';
+import Wordlist from '../built/lib/wordlist';
+
+const testWords = {
+  'example': { lists: [1], matchMethod: Constants.MatchMethods.Exact, repeat: true, sub: 'demo' },
+  'placeholder': { lists: [1, 2], matchMethod: Constants.MatchMethods.Exact, repeat: false, sub: 'variable' },
+  'sample': { lists: [2], matchMethod: Constants.MatchMethods.Partial, repeat: false, sub: 'piece' },
+  'word': { matchMethod: Constants.MatchMethods.Whole, repeat: true, sub: 'idea' }
+};
+
+describe('Wordlist', function() {
+  describe('constructor', function() {
+    it('should build a new wordlist with all words', function() {
+      const wordlist = new Wordlist({ words: testWords }, 0);
+      expect(wordlist.all.length).to.equal(4);
+      expect(wordlist.list).to.eql(['placeholder', 'example', 'sample', 'word']);
+      expect(wordlist.regExps.length).to.equal(4);
+    });
+
+    it('should build a new wordlist assigned to wordlist 1', function() {
+      const wordlist = new Wordlist({ words: testWords }, 1);
+      expect(wordlist.all.length).to.equal(3);
+      expect(wordlist.list).to.eql(['placeholder', 'example', 'word']);
+      expect(wordlist.regExps.length).to.equal(3);
+    });
+  });
+
+  describe('find()', function() {
+    it('by name (string)', function() {
+      const wordlist = new Wordlist({ words: testWords }, 0);
+      expect(wordlist.find('word').sub).to.equal('idea');
+    });
+
+    it('by id', function() {
+      const wordlist = new Wordlist({ words: testWords }, 2);
+      expect(wordlist.find(1).value).to.equal('sample');
+    });
+
+    it('non-existent word', function() {
+      const wordlist = new Wordlist({ words: testWords }, 2);
+      expect(wordlist.find(99)).to.equal(undefined);
+      expect(wordlist.find('non-existent')).to.equal(undefined);
+    });
+  });
+});
