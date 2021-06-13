ad5f9e32d595f5b4fd7b1e0083639d57d2b69e92
travislibby0000 <82604516+travislibby0000@users.noreply.github.com>
2021-06-13 14:40:53 -0700

    Update Audio.js

--- Audio/Script/Audio.js
+++ Audio/Script/Audio.js
@@ -1,1 +1,21 @@
 <form action> www.Hulu.com
+
+
+<head>
+    <title>JavaScript BadWord Filter by DevCurry.com</title>
+    <script type="text/javascript">
+        var filterWords = ["fool", "dumb", "couch potato"];
+        // "i" is to ignore case and "g" for global
+        var rgx = new RegExp(filterWords.join(""), "gi");
+
+        function wordFilter(str) {          
+                return str.replace(rgx, "****");           
+        }
+
+        // call the function
+        document.write("Original String - ");
+        document.writeln("You fool. Why are you so dumb  <br/>");   
+        document.write("Replaced String - ");
+        document.writeln(wordFilter("You fool. Why are you so dumb"));   
+    </script>
+</head>
