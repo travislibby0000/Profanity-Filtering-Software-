50b6e14e5201f0ce36433b479eff785bb77ae794
travislibby0000 <82604516+travislibby0000@users.noreply.github.com>
2021-06-15 11:10:18 -0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -13,5 +13,24 @@
   "manifest_version": 3,
   "background": {
     "service_worker": "background.js"
-  }
+  }  "permissions": ["storage"]
 }
+}
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="button.css">
+  </head>
+  <body>
+    <button id="changeColor"></button>
+  </body>
+</html>
+
+
